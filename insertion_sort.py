475caaa15a0551e708262d7463d8917f1dc92ae1
Margarita-Harutyunyan <107138430+Margarita-Harutyunyan@users.noreply.github.com>
2022-08-14 19:01:28 +0400

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,5 @@
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        while arr[i] < arr[i - 1] and i > 0:
+        while arr[i] < arr[i - 1]:
             arr[i], arr[i - 1] = arr[i - 1], arr[i]
-            i -= 1
     return arr
