78c0cedaff5bc7edb2a28bdecbce6eac6ea26fdd
Margarita-Harutyunyan <107138430+Margarita-Harutyunyan@users.noreply.github.com>
2022-08-15 13:41:40 +0400

    Create largest_palindrome_product.py

---
+++ largest_palindrome_product.py
@@ -0,0 +1,20 @@
+def is_palindrome(n):
+    tmp = n
+    reversed = 0
+    while n > 0:
+        digit = n % 10
+        reversed = reversed * 10 + digit
+        n //= 10
+    if tmp == reversed:
+        return True
+    else:
+        return False
+ls = []
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        n = i * j
+        if is_palindrome(n) and n not in ls:
+            ls.append(n)
+            ls.sort()
+print(ls[-1])
+
